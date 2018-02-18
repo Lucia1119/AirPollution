4266a88cc54642eb097adee5cd4940d3a13eaafd
Lucia <lucia1119@gmail.com>
2018-02-18 16:15:34 +0800

    pollutantmean function

--- AirPollution.R
+++ AirPollution.R
@@ -1,6 +1,15 @@
-# pollutantmean=function(directory,pollutant,id=1:332){}
+# pollutantmean calculates the mean of a pollutant (sulfate or nitrate) 
+# across a specified list of monitors.
+
+# download files
+# file.url="https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip"
+# if (!file.exists("specdata")){
+#         download.file(file.url,destfile = "rprog%2Fdata%2Fspecdata.zip")
+#         unzip(zipfile = "rprog%2Fdata%2Fspecdata.zip")
+# }
 
 
+# define table names
 FormatIdName=function(id){
         sprintf("file%03d",id)
 }
@@ -9,14 +18,41 @@ FileName=function(id=1:332){
         sapply(id,FormatIdName)
 }
 
+
+# read tables
 FormatFilePath=function(directory,id=1:332){
         sprintf(".\\%s\\%03d.csv",directory,id)
 }
 
 ReadFile=function(directory,id=1:332){
         format.filepath=FormatFilePath(directory,id)
-        sapply(format.filepath,read.csv)
+        lapply(format.filepath,read.csv)
         #read.csv(sprintf(".\\specdata\\%03d.csv",id),stringsAsFactors = FALSE)
 }
 
 
+
+# calculate pollutant means
+# sulfate or nitrate
+GetPollutantMean=function(dfList,fileName,pollutant){
+        pollutantValue=dfList[[fileName]][[pollutant]]
+        mean(pollutantValue,na.rm = TRUE)
+}
+
+GetPollutantMeanAll=function(dfList,fileName,pollutant){
+        pollutantMeanList=sapply(fileName,GetPollutantMean, 
+                                 dfList=dfList,pollutant=pollutant)
+        #pollutantMeanList
+        mean(pollutantMeanList,na.rm = TRUE)
+}
+
+
+# return the mean of a pollutant (sulfate or nitrate) 
+# across a specified list of monitors.
+pollutantmean=function(directory,id=1:332,pollutant){
+        read.file=ReadFile(directory,id)
+        file.name=FileName(id)
+        names(read.file)=file.name
+        GetPollutantMeanAll(read.file,file.name,pollutant)
+}
+        
